fa19bb87ddbffe2189756cff18521d7354dfe1f1
VARUN KAPASIA <96992611+KAPASIAVar@users.noreply.github.com>
2023-10-28 22:31:42 +0530

    Update Balanced_parenthesis.cpp
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    
    Example 1:
    
    Input: s = "()"
    Output: true

--- Balanced_parenthesis.cpp
+++ Balanced_parenthesis.cpp
@@ -1,27 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool solve(string x){
-	stack<int>st;
-	for(int i=0;i<x.size();i++){
-		if(st.empty())st.push(x[i]);
-	    else if((x[i]==')' && st.top()=='(') || (x[i]=='}' && st.top()=='{') || (x[i]==']' && st.top()=='[' ) ){
-			st.pop();
-		}
-		else st.push(x[i]);
-	}
-	if(st.empty()){
-		return true;
-	}
-	else return false;
+bool is_valid(string inp)
+{
+    stack<char> st;
+    for (auto i : inp)
+    {
+        if (st.empty())
+            st.push(i);
+        else if(st.top()=='[' && i==']')st.pop();
+        else if(st.top()=='{' && i=='}')st.pop();
+        else if(st.top()=='(' && i==')')st.pop();
+        else st.push(i);
 
+    }
+    if(st.empty())return 1;
+    return 0;
 }
-int main() {
-	string s;
-	cin>>s;
-	if(solve(s)){
-		cout<<"Yes";
-	}
-	else cout<<"No";
-
-	return 0;
+int main()
+{
+    string st;
+    cin>>st;
+    if(is_valid(st))cout<<"Balanced\n"<<"\t--End--"<<endl;
+    else cout<<"Not Balanced"<<"\t--End--"<<endl;
 }
